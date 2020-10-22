f849026192a8963ebba0622ab3f7a51218ba3e5e
bob8677 <68208828+bob8677@users.noreply.github.com>
2020-10-22 15:12:04 -0500

    Update pam.py

--- src/pam.py
+++ src/pam.py
@@ -32,10 +32,10 @@ def doAuth(pamh):
 			return pamh.PAM_AUTHINFO_UNAVAIL
 
         # Abort if the video device does not exist
-        if not os.path.exists(config.get("video", "device_path")):
-                if config.getboolean("video", "warn_no_device"):
-                        print("Camera path is not configured correctly, please edit the 'device_path' config value.")
-                return pamh.PAM_AUTHINFO_UNAVAIL
+	if not os.path.exists(config.get("video", "device_path")):
+		if config.getboolean("video", "warn_no_device"):
+			print("Camera path is not configured correctly, please edit the 'device_path' config value.")
+		return pamh.PAM_AUTHINFO_UNAVAIL
 
 	# Set up syslog
 	syslog.openlog("[HOWDY]", 0, syslog.LOG_AUTH)
