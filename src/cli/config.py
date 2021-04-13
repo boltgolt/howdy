8d299c2630f450e7c92b09756c0f4f1d4cc3c031
wyapx <a1025952421@outlook.com>
2021-04-14 01:07:40 +0800

    add more editor

--- src/cli/config.py
+++ src/cli/config.py
@@ -7,14 +7,16 @@ import subprocess
 # Let the user know what we're doing
 print("Opening config.ini in the default editor")
 
-# Default to the nano editor
-editor = "/bin/nano"
-
 # Use the user preferred editor if available
 if "EDITOR" in os.environ:
-	editor = os.environ["EDITOR"]
+    editor = os.environ["EDITOR"]
 elif os.path.isfile("/etc/alternatives/editor"):
-	editor = "/etc/alternatives/editor"
+    editor = "/etc/alternatives/editor"
+elif os.path.isfile("/usr/bin/nano"):
+    editor = "/usr/bin/nano"
+else:
+    # Default to the vi editor
+    editor = "/usr/bin/vi"
 
 # Open the editor as a subprocess and fork it
 subprocess.call([editor, os.path.dirname(os.path.realpath(__file__)) + "/../config.ini"])
